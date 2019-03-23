2e3cb6994eb20e56a3263045267d376d381c07bb
Karl Lurman <karllurman@nbnco.com.au>
2019-03-23 12:45:56 +1100

    Sim updated to have a clock tick handle function, so that we can set things in the register on particular clock ticks. logging improved in sim too. More Beetle code added to top.

--- sim_main.cpp
+++ sim_main.cpp
@@ -4,31 +4,113 @@
 #include "verilated.h"
 #include <iostream>
 
-int main(int argc, char **argv, char **env) {
+// Here we can do specific things on certain clock ticks
+void handlePositiveEdgeClock(int tick, Vtop* top) {
+    // This could be something smarter, but for now... we only care about a few clock cycles
+    if (tick == 1) {
+        // @todo stuff on first tick
+    }
+}
+
+// Here we can do specific things on certain clock ticks
+void logCdRegisters(Vtop* top) {
+    printf("PCE_CD: 0x00 CDC_STAT:    %02x", top->r_CDStatus);
+    if (top->r_CDStatus & 0x80) printf(" [7]BUSY ");
+    if (top->r_CDStatus & 0x40) printf(" [6]REQ  ");
+    if (top->r_CDStatus & 0x20) printf(" [5]MSG  ");
+    if (top->r_CDStatus & 0x10) printf(" [4]CD   ");
+    if (top->r_CDStatus & 0x08) printf(" [3]IO   ");
+    printf("\n");
+
+    printf("PCE_CD: 0x02 INT_MASK:    %02x", top->r_CDIntMask);
+    if (top->r_CDIntMask & 0x80) printf(" [7]ACK_FLAG! ");    // Actual ACKnowledge flag, AFAIK. ElectronAsh.
+    if (top->r_CDIntMask & 0x40) printf(" [6]ACK_MASK  ");
+    if (top->r_CDIntMask & 0x20) printf(" [5]DONE_MASK ");
+    if (top->r_CDIntMask & 0x10) printf(" [4]BRAM_MASK ");
+    if (top->r_CDIntMask & 0x08) printf(" [3]FULL_MASK ");
+    if (top->r_CDIntMask & 0x04) printf(" [2]HALF_MASK ");
+    if (top->r_CDIntMask & 0x02) printf(" [1]L/R ");
+    printf("\n");
+    
+    printf("PCE_CD: 0x03 BRAM_LOCK:   %02x", top->r_CDBRAMLock);
+    if (top->r_CDBRAMLock & 0x40) printf(" ACK  "); // Bit 6.
+    if (top->r_CDBRAMLock & 0x20) printf(" DONE "); // Bit 5.
+    if (top->r_CDBRAMLock & 0x10) printf(" BRAM "); // Bit 4.
+    if (top->r_CDBRAMLock & 0x08) printf(" FULL "); // Bit 3.
+    if (top->r_CDBRAMLock & 0x04) printf(" HALF "); // Bit 2.
+    if (top->r_CDBRAMLock & 0x02) printf(" L/R ");  // Bit 1.
+    printf("\n");
+
+    // 	data = read_cd_reg(0x04);
+    // 	if (previous_reg_04 != data) {
+    // 		printf("PCE_CD: 0x04 CD RESET:    %02x\n", data);
+    // 		previous_reg_04 = data;
+    // 	}
+
+    // 	data = read_cd_reg(0x05);
+    // 	if (previous_reg_05 != data) {
+    // 		printf("PCE_CD: 0x05 CONV_PCM:    %02x\n", data);
+    // 		previous_reg_05 = data;
+    // 	}
+
+    // 	data = read_cd_reg(0x06);
+    // 	if (previous_reg_06 != data) {
+    // 		printf("PCE_CD: 0x06 PCM_DATA:    %02x\n", data);
+    // 		previous_reg_06 = data;
+    // 	}
+
+    // 	data = read_cd_reg(0x07);
+    // 	if (previous_reg_07 != data) {
+    // 		printf("PCE_CD: 0x07 BRAM_UNLOCK: %02x\n", data);
+    // 		previous_reg_07 = data;
+    // 	}
+
+    // 	// todo: need to complete all of these
+    // 	// printf("0x00 CDC_STAT:    %02x\n", read_cd_reg(0x00));
+    // 	// printf("0x01 CDC_CMD:     %02x\n", read_cd_reg(0x01));
+    // 	// printf("0x02 INT_MASK:    %02x\n", read_cd_reg(0x02));
+    // 	// printf("0x03 BRAM_LOCK:   %02x\n", read_cd_reg(0x03));
+    // 	// printf("0x04 CD RESET:    %02x\n", read_cd_reg(0x04));
+    // 	// printf("0x05 CONV_PCM:    %02x\n", read_cd_reg(0x05));
+    // 	// printf("0x06 PCM_DATA:    %02x\n", read_cd_reg(0x06));
+    // 	// printf("0x07 BRAM_UNLOCK: %02x\n", read_cd_reg(0x07));
+    // 	// printf("0x08 ADPCM_A_LO:  %02x\n", read_cd_reg(0x08));
+    // 	// printf("0x09 ADPCM_A_HI:  %02x\n", read_cd_reg(0x09));
+    // 	// printf("0x0a AD_RAM_DATA: %02x\n", read_cd_reg(0x0a));
+    // 	// printf("0x0b AD_DMA_CONT: %02x\n", read_cd_reg(0x0b));
+    // 	// printf("0x0c ADPCM_STAT:  %02x\n", read_cd_reg(0x0c));
+    // 	// printf("0x0d ADPCM_ADDR:  %02x\n", read_cd_reg(0x0d));
+    // 	// printf("0x0e ADPCM_RATE:  %02x\n", read_cd_reg(0x0e));
+    // 	// printf("0x0f ADPCM_FADE:  %02x\n", read_cd_reg(0x0f));
+}
+
+int main(int argc, char **argv, char **env) {    
     Verilated::commandArgs(argc, argv);
     // Create instance
     Vtop* top = new Vtop;
 
     // Simulation time...
-    
+    int tick = 0;
     while (!Verilated::gotFinish()) {
+        tick++;
         char input;
         cin>>input;
-        cout << 't' << 'i' << 'c' << 'k' << endl;
-        // Toggle the clock
+        // Toggle the clock - tick tock
         top->i_clk = 0;
         top->eval();
+        // tick
         top->i_clk = 1;
+        handlePositiveEdgeClock(tick, top);
         top->eval();
+        logCdRegisters(top);
+        // tock
         top->i_clk = 0;
         top->eval();
-        // Read a output
-        //cout << top->r_CDStatus << endl;
     }
 
     // Done simulating
     top->final();
-    // Cleanup
+    // Clean-up
     delete top;
     exit(0);
 }
--- top.v
+++ top.v
@@ -1,14 +1,31 @@
-module top(input i_clk, output r_CDStatus, output o_GOTCDCOMMAND);
+module top(
+    input i_clk, 
+    inout r_CDStatus,       // 0x00 
+    inout r_CDComand,       // 0x01
+    inout r_CDIntMask,      // 0x02
+    inout r_CDBRAMLock,     // 0x03
+    output o_GOTCDCOMMAND
+);
     // These will eventually be external registers
     reg [7:0] r_CDStatus = 0;
+    reg [7:0] r_CDCommand = 0;
+    reg [7:0] r_CDBRAMLock = 0;
+    // Debug
     reg [0:0] o_GOTCDCOMMAND = 0;
     //reg [7:0] r_CDIntMask;
     
     // Internal state
     reg [7:0] CurrentPhase = 0;
+    // I believe this is the command bus
+    reg [7:0] CDBusDb = 0;
     reg [0:0] CDLastReset = 0;
     reg [0:0] CDStatusSent = 0;
     reg [0:0] CDMessageSent = 0;
+
+    localparam true = 1;
+    localparam false = 0;
+    localparam IRQ_8000 = 1;
+    localparam IRQ_No_8000 = 0;
 
     localparam PHASE_BUS_FREE    = 8'b00000000;
     localparam PHASE_COMMAND     = 8'b00000001;
@@ -18,8 +35,9 @@ module top(input i_clk, output r_CDStatus, output o_GOTCDCOMMAND);
     localparam PHASE_MESSAGE_IN  = 8'b00010000;
     localparam PHASE_MESSAGE_OUT = 8'b00100000;
 
-    localparam true = 1;
-    localparam false = 0;
+    localparam PCECD_Drive_IRQ_DATA_TRANSFER_DONE = 3'b001;
+    localparam PCECD_Drive_IRQ_DATA_TRANSFER_READY = 3'b010;
+    localparam PCECD_Drive_IRQ_MAGICAL_REQ = 3'b011;
 
     // CD Interface Register 0x00 - CDC status
     // x--- ---- busy signal
@@ -39,75 +57,103 @@ module top(input i_clk, output r_CDStatus, output o_GOTCDCOMMAND);
     function MSG_signal; MSG_signal = r_CDStatus[5]; endfunction
     function CD_signal; CD_signal = r_CDStatus[4]; endfunction
     function IO_signal; IO_signal = r_CDStatus[3]; endfunction
-
     task SetREQ; input [0:0] bitValue; r_CDStatus[6] = bitValue; endtask
 
-    // Signals under the control of the initiator(not us!)
+    // Signals under the control of the initiator (not us!)
     localparam PCECD_Drive_kingRST_mask	= 16'h020;
     localparam PCECD_Drive_kingACK_mask = 16'h040;
     localparam PCECD_Drive_kingSEL_mask	= 16'h100;
 
-    // Internal Signals 
+    // Internal Signals (IOP in beetle)
     reg [15:0] cd_bus_signals;
-
-    function RST_signal; RST_signal = cd_bus_signals[5] ; endfunction
+    function RST_signal; RST_signal = cd_bus_signals[5]; endfunction
     function ACK_signal; ACK_signal = cd_bus_signals[6]; endfunction
-    // @todo assume a cd device has been selected, but this would be bit cd_bus_signals[8]
-    function SEL_signal; SEL_signal = 1; endfunction
+    function SEL_signal; SEL_signal = cd_bus_signals[8]; endfunction
+
+    task SetkingRST; input [0:0] bitValue; cd_bus_signals[5] = bitValue; endtask
+    task PCECD_Drive_SetRST; input [0:0] bitValue; SetkingRST(bitValue); endtask
+    task SetkingACK; input [0:0] bitValue; cd_bus_signals[6] = bitValue; endtask
+    task PCECD_Drive_SetACK; input [0:0] bitValue; SetkingACK(bitValue); endtask
+    task SetkingSEL; input [0:0] bitValue; cd_bus_signals[8] = bitValue; endtask
+    task PCECD_Drive_SetSEL; input [0:0] bitValue; SetkingSEL(bitValue); endtask
+
+    // Command bus getter/setters?
+    function PCECD_Drive_GetDB; PCECD_Drive_GetDB = CDBusDb; endfunction
+    task PCECD_Drive_SetDB; input [7:0] dbValue; CDBusDb = dbValue; endtask
+
+    task PCECD_Drive_Power;
+        CurrentPhase = PHASE_BUS_FREE;
+        VirtualReset;
+    endtask;
+
+    task CDIRQ;
+        input [0:0] mode; // 8000 is used to do different things to the IRQ register by pce_fast... this replicates that
+        input [2:0] irqType;
+        begin
+            if (mode) begin
+                if (irqType == PCECD_Drive_IRQ_DATA_TRANSFER_DONE) begin
+                    r_CDBRAMLock &= ~8'h20;
+                end else if (irqType == PCECD_Drive_IRQ_DATA_TRANSFER_READY) begin
+                    r_CDBRAMLock &= ~8'h40;
+                end
+            end else if (irqType == PCECD_Drive_IRQ_DATA_TRANSFER_DONE) begin
+                r_CDBRAMLock |= 8'h20;
+            end else if (irqType == PCECD_Drive_IRQ_DATA_TRANSFER_READY) begin
+                r_CDBRAMLock |= 8'h40;
+            end
+            // @todo update_irq_state();
+        end
+    endtask
 
     task ChangePhase;
         input [7:0] new_phase;
         begin
-            if (new_phase == PHASE_BUS_FREE)
-                begin
-                    $display ("PHASE_BUS_FREE");
-                    r_CDStatus = r_CDStatus & ~BUSY_BIT & ~MSG_BIT & ~CD_BIT & ~IO_BIT & ~REQ_BIT;
-                    // @todo Need to do some kind of interrupt here
-                    CurrentPhase = PHASE_BUS_FREE;
-                end
-            if (new_phase == PHASE_DATA_IN)
-                begin
-                    $display ("PHASE_DATA_IN");
-                    r_CDStatus = r_CDStatus | BUSY_BIT | IO_BIT & ~MSG_BIT & ~CD_BIT & ~REQ_BIT;
-                    CurrentPhase = PHASE_DATA_IN;
-                end
-            if (new_phase == PHASE_STATUS)
-                begin
-                    $display ("PHASE_STATUS");
-                    r_CDStatus = r_CDStatus | BUSY_BIT | CD_BIT | IO_BIT | REQ_BIT & ~MSG_BIT;
-                    CurrentPhase = PHASE_STATUS;
-                end
-            if (new_phase == PHASE_MESSAGE_IN)
-                begin
-                    $display ("PHASE_MESSAGE_IN");
-                    r_CDStatus = r_CDStatus | BUSY_BIT | MSG_BIT | CD_BIT | IO_BIT | REQ_BIT;
-                    CurrentPhase = PHASE_MESSAGE_IN;
-                end
-            if (new_phase == PHASE_COMMAND)
-                begin
-                    $display ("PHASE_COMMAND");
-                    r_CDStatus = r_CDStatus | BUSY_BIT | CD_BIT | REQ_BIT & ~IO_BIT & ~MSG_BIT;
-                    CurrentPhase = PHASE_COMMAND;
-                end
+            if (new_phase == PHASE_BUS_FREE) begin
+                $display ("PHASE_BUS_FREE");
+                r_CDStatus = r_CDStatus & ~BUSY_BIT & ~MSG_BIT & ~CD_BIT & ~IO_BIT & ~REQ_BIT;
+                CDIRQ(IRQ_8000, PCECD_Drive_IRQ_DATA_TRANSFER_DONE);
+                CurrentPhase = PHASE_BUS_FREE;
+            end
+            if (new_phase == PHASE_DATA_IN) begin
+                $display ("PHASE_DATA_IN");
+                r_CDStatus = r_CDStatus | BUSY_BIT | IO_BIT & ~MSG_BIT & ~CD_BIT & ~REQ_BIT;
+                CurrentPhase = PHASE_DATA_IN;
+            end
+            if (new_phase == PHASE_STATUS) begin
+                $display ("PHASE_STATUS");
+                r_CDStatus = r_CDStatus | BUSY_BIT | CD_BIT | IO_BIT | REQ_BIT & ~MSG_BIT;
+                CurrentPhase = PHASE_STATUS;
+            end
+            if (new_phase == PHASE_MESSAGE_IN) begin
+                $display ("PHASE_MESSAGE_IN");
+                r_CDStatus = r_CDStatus | BUSY_BIT | MSG_BIT | CD_BIT | IO_BIT | REQ_BIT;
+                CurrentPhase = PHASE_MESSAGE_IN;
+            end
+            if (new_phase == PHASE_COMMAND) begin
+                $display ("PHASE_COMMAND");
+                r_CDStatus = r_CDStatus | BUSY_BIT | CD_BIT | REQ_BIT & ~IO_BIT & ~MSG_BIT;
+                CurrentPhase = PHASE_COMMAND;
+            end
         end
     endtask
 
     task VirtualReset;
         $display("VirtualReset");
-        // @todo reset the cd drive - i.e anything internal to this module
+        r_CDStatus = 0;
+        o_GOTCDCOMMAND = 0;
+        CurrentPhase = 0;
+        CDLastReset = 0;
+        CDStatusSent = 0;
+        CDMessageSent = 0;
         ChangePhase(PHASE_BUS_FREE);
-    endtask
-    
-    task LogRegisers;
-        $display ("PCE_CD: 0X00 CDC_STAT: %h", r_CDStatus);
     endtask
 
     task RunCDRead;
-        $display ("PCE_CD: RunCDRead - TBC");
+        //$display ("PCE_CD: RunCDRead - TBC");
     endtask
 
     task RunCDDA;
-        $display ("PCE_CD: RunCDDA - TBC");
+        //$display ("PCE_CD: RunCDDA - TBC");
     endtask
 
     task PCECD_Drive_Run;
@@ -199,7 +245,19 @@ module top(input i_clk, output r_CDStatus, output o_GOTCDCOMMAND);
         end
     endtask
 
-    always @ (posedge i_clk) begin
+    task PCECD_Run;
+        // @todo ClearACKDelay?
+        PCECD_Drive_SetACK(false);
         PCECD_Drive_Run;
+        // @todo ADPCM stuff
+        // @todo Fadeouts/ins
+    endtask
+
+    always @ (posedge i_clk) begin
+        PCECD_Run;
+    end
+
+    initial begin
+        PCECD_Drive_Power;
     end
 endmodule
